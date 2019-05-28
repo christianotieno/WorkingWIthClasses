0cf36b4c2b22541abb587d66381ac7ad8c8aaf55
Christian <cgao273@gmail.com>
2019-05-28 18:48:33 +0300

    Working WIth Classes

---
+++ classes_and_instances.py
@@ -0,0 +1,13 @@
+class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+emp_1 = Employee('John', 'Doe', 34000)
+emp_2 = Employee('Jane', 'Doe', 45000)
